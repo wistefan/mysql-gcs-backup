f2f77659c37b7d7552408532c1705846f6e14039
Stefan Wiedemann <wistefan@googlemail.com>
2021-05-10 08:01:21 +0200

    quote password and username

--- sh/backup-to-gcs.sh
+++ sh/backup-to-gcs.sh
@@ -4,8 +4,7 @@ echo "Run backup"
 date=$(date "+%Y-%m-%dT%H:%M:%SZ")
 filename=${BACKUP_FILE_PREFIX}-backup-${date}
 
-echo "mysqldump -h ${MYSQL_HOST} -P ${MYSQL_PORT} -u ${MYSQL_USER} -p${MYSQL_PASSWORD} --all-databases > ${filename}"
-mysqldump -h "${MYSQL_HOST}" -P "${MYSQL_PORT}" -u ${MYSQL_USER} -p${MYSQL_PASSWORD} --all-databases > "${filename}"
+mysqldump -h "${MYSQL_HOST}" -P "${MYSQL_PORT}" -u"${MYSQL_USER}" -p"${MYSQL_PASSWORD}" --all-databases > "${filename}"
 
 tar -czvf ${filename}.tar.gz ${filename}
 
