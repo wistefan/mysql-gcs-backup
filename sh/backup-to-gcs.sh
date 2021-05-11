8682a4fafa29f88188f94add9e9210814cebc379
Stefan Wiedemann <wistefan@googlemail.com>
2021-05-11 09:51:39 +0200

    fix mysql backup

--- sh/backup-to-gcs.sh
+++ sh/backup-to-gcs.sh
@@ -4,11 +4,11 @@ echo "Run backup"
 date=$(date "+%Y-%m-%dT%H:%M:%SZ")
 filename=${BACKUP_FILE_PREFIX}-backup-${date}
 
-mysqldump -h "${MYSQL_HOST}" -P "${MYSQL_PORT}" -u"${MYSQL_USER}" -p"${MYSQL_PASSWORD}" --all-databases > "${filename}"
+mysqldump -h "${MYSQL_HOST}" -P "${MYSQL_PORT}" -u"${MYSQL_USER}" -p"${MYSQL_PASSWORD}" --all-databases > "/temp/${filename}"
 
-tar -czvf ${filename}.tar.gz ${filename}
+tar -czvf /temp/${filename}.tar.gz /temp/${filename}
 
 gcloud auth activate-service-account --key-file=${GCS_KEYFILE_PATH}
-gsutil cp /temp/${filename}.tar.gz ${GCS_BUCKET}/${filename}.tar.gz
+gsutil -o "GSUtil:state_dir=/temp/.gsutil" cp /temp/${filename}.tar.gz ${GCS_BUCKET}/${filename}.tar.gz
 
 echo "Finished backup"
